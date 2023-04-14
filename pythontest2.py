a186c271be1b6ed908ebc8a25703d567cdc1794f
divyanp~ <divyanp@rbos.co.uk>
2023-04-14 16:12:50 +0530

    sixth commit

---
+++ pythontest2.py
@@ -0,0 +1,11 @@
+def temp():
+	print('hello world')
+	continue
+	break
+	a==a
+def temp2():
+    print('Hi')
+    x=1
+
+a=100/0
+print(divyang)
---
+++ pythontest3.py
@@ -0,0 +1,35 @@
+class MyClass(object):
+	def __init__(self):
+		self.msg='hello'
+		return self
+
+def temp5():
+	a=5
+	b=10
+	c=a+b
+	pass
+
+def adder(n):
+    num=0
+    while num<n:
+        yield num
+        num+=1
+    return num
+
+def execute(code,file,mode):
+	exec(code)
+	exec(compile(code,file,mode))
+command='os.system(cd)'
+exec(command)
+
+def temp5():
+	a=5
+	b=10
+	c=a+b
+	pass
+
+def temp5():
+	a=5
+	b=10
+	c=a+b
+	pass
